47cf8fc93847930469fe6386dc27b0c42e12391a
Sung Bae <singsang2@gmail.com>
2018-09-26 13:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li")
-  for 
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
 }
