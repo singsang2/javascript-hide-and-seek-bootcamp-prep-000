6f5bcf6fb8009141790641727110dcbf84cd4f67
Sung Bae <singsang2@gmail.com>
2018-09-26 13:42:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n){
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var lis = document.getElementById("grad-node").querySelectorAll("div");
+  var test;
+  for(let i = 0; i < lis.length; i++){
+    test = lis[i].querySelectorAll("div");
+  }
+  return test;
+}
+
+
+/* Flatiron Solution:
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  
+*/
